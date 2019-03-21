097edc02291a62195d3e46324173689875b086e8
NolanGougeon <gnolan@hotmail.fr>
2019-03-21 21:27:12 +0100

    [DAO] Création de getAllParametre

--- sw/dbconnexion.php
+++ sw/dbconnexion.php
@@ -1,32 +1,30 @@
 <?php
+function connectionToBD(){
+    try
+    {
 
-    function connectionToBD(){
-        try
-        {
+        $configs_env = file_get_contents('../config.json');
+        $configs_env = json_decode($configs_env,true);
 
-            $configs_env = file_get_contents('../config.json');
-            $configs_env = json_decode($configs_env,true);
+        $USERS_db=  $configs_env['user'];
+        $PASSWORD_db= $configs_env['password'];
+        $HOST_db= $configs_env['hostname'];
+        $NAME_db= $configs_env['dbname'];
 
-            $USERS_db=  $configs_env['user'];
-            $PASSWORD_db= $configs_env['password'];
-            $HOST_db= $configs_env['hostname'];
-            $NAME_db= $configs_env['dbname'];
-
-            $bdconect = new PDO("mysql:host=".$HOST_db.";dbname=".$NAME_db,
-                $USERS_db, $PASSWORD_db, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES UTF8")
-            );
-            // $bdd = new PDO("mysql:host=".$host.";dbname=".$dbname."", $user, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES UTF8"));
-            // echo 'base de donnees connectee';
-            return $bdconect;
-        }catch(Exception $e){
-            echo "Erreur: ".$e->getMessage();
-            die("<h2>Impossible de se connecter à la base de données !</h2>");
-        }
-
+        $bdconect = new PDO("mysql:host=".$HOST_db.";dbname=".$NAME_db,
+            $USERS_db, $PASSWORD_db, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES UTF8")
+        );
+        // $bdd = new PDO("mysql:host=".$host.";dbname=".$dbname."", $user, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES UTF8"));
+        // echo 'base de donnees connectee';
+        return $bdconect;
+    }catch(Exception $e){
+        echo "Erreur: ".$e->getMessage();
+        die("<h2>Impossible de se connecter à la base de données !</h2>");
     }
 
-function sendMail($email,$object,$contenu){
+}
 
+function sendMail($email,$object,$contenu){
 $header="MIME-Version: 1.0\r\n";
 	$header.='From:"GlazikGym.com"<support@glazikgym.com>'."\n";
 	$header.='Content-Type:text/html; charset="uft-8"'."\n";
@@ -79,5 +77,12 @@ $header="MIME-Version: 1.0\r\n";
         </body>
 </html> ";
 	mail($email, $object, $message, $header);
+}
 
+function getAllParametre(){
+    $connexion = connectionToBD();
+    $query = $connexion->prepare("SELECT * FROM parametre");
+    $query->execute();
+
+    return $result = $query->fetch();
 }
